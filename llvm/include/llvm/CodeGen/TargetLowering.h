c2d29814ff13159741ff8674dfcc54c67cf21aa5
Ivan Belokobylskiy <belokobylskij@gmail.com>
2026-04-19 01:48:11 +0400

    CodeGen: restore jump-table legality checks

--- llvm/include/llvm/CodeGen/TargetLowering.h
+++ llvm/include/llvm/CodeGen/TargetLowering.h
@@ -1425,8 +1425,6 @@ public:
 
   /// Return true if lowering to a jump table is allowed.
   virtual bool areJTsAllowed(const Function *Fn) const {
-    return false;
-
     if (Fn->getFnAttribute("no-jump-tables").getValueAsBool())
       return false;
 
