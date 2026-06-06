f4856fe098689ceef0543528070c3b4ebc24752a
Ivan Belokobylskiy <belokobylskij@gmail.com>
2026-06-06 17:11:55 +0400

    lld: preserve Thumb bit in tc32 long thunks

--- lld/test/ELF/tc32-thunk.s
+++ lld/test/ELF/tc32-thunk.s
@@ -2,23 +2,27 @@
 # RUN: rm -rf %t && split-file %s %t
 # RUN: llvm-mc -triple=tc32-unknown-none-elf -filetype=obj %t/tc32-thunk.s -o %t/tc32-thunk.o
 # RUN: ld.lld -T %t/tc32-thunk.lds %t/tc32-thunk.o -o %t/tc32-thunk
-# RUN: llvm-objdump --triple=tc32 -d %t/tc32-thunk | FileCheck %s
+# RUN: llvm-readelf -s %t/tc32-thunk | FileCheck %s --check-prefix=SYMS
+# RUN: llvm-objdump --triple=tc32 -d %t/tc32-thunk | FileCheck %s --check-prefix=DISASM
 
-# CHECK-LABEL: <_start>:
-# CHECK-NEXT:    {{[0-9a-f]+}}: 00 90 02 98   tjl 0x{{[0-9a-f]+}} <__TC32ABSLongThunk_target> @ imm = #0x4
-# CHECK-NEXT:    {{[0-9a-f]+}}: 70 07         tjex lr
-# CHECK:       {{[0-9a-f]+}} <__TC32ABSLongThunk_target>:
-# CHECK-NEXT:    {{[0-9a-f]+}}: 03 64         tpush {r0, r1}
-# CHECK-NEXT:    {{[0-9a-f]+}}: 04 08         tloadr r0, [pc, #0x10]
-# CHECK-NEXT:    {{[0-9a-f]+}}: c0 06         nop
-# CHECK-NEXT:    {{[0-9a-f]+}}: c0 06         nop
-# CHECK-NEXT:    {{[0-9a-f]+}}: 01 30         tstorer r0, [sp, #0x4]
-# CHECK-NEXT:    {{[0-9a-f]+}}: c0 06         nop
-# CHECK-NEXT:    {{[0-9a-f]+}}: c0 06         nop
-# CHECK-NEXT:    {{[0-9a-f]+}}: 01 6d         tpop {r0, pc}
-# CHECK-NEXT:    {{[0-9a-f]+}}: c0 06         nop
-# CHECK-NEXT:    {{[0-9a-f]+}}: c0 06         nop
-# CHECK-NEXT:    {{[0-9a-f]+}}: 00 00 43 00   .word 0x00430000
+# SYMS:      Symbol table '.symtab' contains
+# SYMS:      {{[0-9]+}}: 00430001     0 FUNC    LOCAL  DEFAULT     2 target
+
+# DISASM-LABEL: <_start>:
+# DISASM-NEXT:    {{[0-9a-f]+}}: 00 90 02 98   tjl 0x{{[0-9a-f]+}} <__TC32ABSLongThunk_target> @ imm = #0x4
+# DISASM-NEXT:    {{[0-9a-f]+}}: 70 07         tjex lr
+# DISASM:       {{[0-9a-f]+}} <__TC32ABSLongThunk_target>:
+# DISASM-NEXT:    {{[0-9a-f]+}}: 03 64         tpush {r0, r1}
+# DISASM-NEXT:    {{[0-9a-f]+}}: 04 08         tloadr r0, [pc, #0x10]
+# DISASM-NEXT:    {{[0-9a-f]+}}: c0 06         nop
+# DISASM-NEXT:    {{[0-9a-f]+}}: c0 06         nop
+# DISASM-NEXT:    {{[0-9a-f]+}}: 01 30         tstorer r0, [sp, #0x4]
+# DISASM-NEXT:    {{[0-9a-f]+}}: c0 06         nop
+# DISASM-NEXT:    {{[0-9a-f]+}}: c0 06         nop
+# DISASM-NEXT:    {{[0-9a-f]+}}: 01 6d         tpop {r0, pc}
+# DISASM-NEXT:    {{[0-9a-f]+}}: c0 06         nop
+# DISASM-NEXT:    {{[0-9a-f]+}}: c0 06         nop
+# DISASM-NEXT:    {{[0-9a-f]+}}: 01 00 43 00   .word 0x00430001
 
 #--- tc32-thunk.s
   .syntax unified
